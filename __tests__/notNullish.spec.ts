bcad9739642042c6b56c5f574f40e920f2e11025
Alexey Solovyov <plotinka@lyoha.info>
2021-11-16 08:19:49 +0300

    fix: удалил консоль лог из теста notNullish.spec.ts

--- __tests__/notNullish.spec.ts
+++ __tests__/notNullish.spec.ts
@@ -6,7 +6,6 @@ const expectFalseArray: unknown[] = [null, undefined, NaN];
 describe('Not nullish', function () {
     test('Works correctly', () => {
         expectTrueArray.forEach((v) => {
-            console.log(v);
             expect(notNullish(v)).toBeTruthy();
         });
         expectFalseArray.forEach((v) => expect(notNullish(v)).toBeFalsy());
